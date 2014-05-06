cc8f3cff76becb8737b5babe85135242990c6f4f
nickkruiter <n.h.w.kruiter@student.utwente.nl>
2014-05-06 16:08:21 +0200

    Week 3

---
+++ session2/result1.sql
@@ -0,0 +1,50 @@
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(a, false, "verschillende voertuigen kunnen dezelfde capaciteit hebben");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(b, true, "een voertuig heeft maar 1 capaciteit");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(c, false, "een onvanger kan meerdere adressen hebben");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(d, true, "elk pakje heeft 1 afleveradrers en 1 ontvanger");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(e, true, "bij een pakje hoort 1 ontvanger en 1 bedrag");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(f, false, "bij een bezorger hoort niet per se 1 voertuig");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(g, true, "er kunnen niet meerdere bezorgers bij een voertuig met bepaalde capaciteit horen");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(h, true, "V projecteert B");
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(i, true, "V projecteert C");
+
+INSERT INTO problem1
+ (task, result, motivation)
+ VALUES
+(j, true, "V projecteert B ecn C");
+
